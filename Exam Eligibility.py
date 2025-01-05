c9a8a1c2d8ccf5186a73ba2b04655b31cb825ab6
Devanshu-lang <136570121+Devanshu-lang@users.noreply.github.com>
2025-01-05 18:33:15 +0530

    Update Exam Eligibility.py

--- Exam Eligibility.py
+++ Exam Eligibility.py
@@ -4,7 +4,7 @@ Exam = "UPSC" # Name of the exam
 
 # Name of the candidate appearing for the exam.
 
-name = input("What is your name ? ")
+name = input("What is your name? ")
 
 # What is the age of the candidate
 
@@ -12,14 +12,13 @@ age = int(input("What's your age? "))
 
 # If the candidate's age is 21 or older, they are eligible.
 
-if age >= 21: 
+if age >= 21:  
      print("You are eligible for giving the " + Exam, "exam.")
 
 # The candidate is not eligible to take this exam. However, they may qualify for other exams that match the age criteria.
 
-elif age >=18:
-     print("You are not eligible for this " + Exam, "exam, but alternatively can you fill other exam forms for whom \
-           the age criteria is below 21")
+elif age >=18 and age <=21:
+     print("You are not eligible for this " + Exam, "exam, but alternatively can you fill other exam forms for whom the age criteria is below 21")
      
 # The candidate is not eligible to appear in any of the exams
 
